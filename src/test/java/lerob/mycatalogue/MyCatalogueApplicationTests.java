2209cfaceb71932b7e4d6641dec3e6a7abc9dd9a
Lerob <njeunkweborel@gmail.com>
2023-09-08 19:12:40 +0100

    first commit

---
+++ src/test/java/lerob/mycatalogue/MyCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package lerob.mycatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCatalogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
